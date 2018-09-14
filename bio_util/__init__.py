860f81e286730e595a18343c4d741b85b354365f
Vinicio Reynoso <vr1087@gmail.com>
2018-09-14 11:38:20 -0500

    Bump version: 0.0.1 → 0.0.2

--- bio_util/__init__.py
+++ bio_util/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Vinicio Reynoso"""
 __email__ = 'vr1087@gmail.com'
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
 from .bio_util import load_references
 from .shell import run_cmd
